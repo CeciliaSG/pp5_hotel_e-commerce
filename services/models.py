8855a465ec2e23a75019cc41045b955cf17a07e6
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-05-19 14:01:42 +0000

    Add models for SpaCaegory and SpaService to models.py in Service app

--- services/models.py
+++ services/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class SpaService(models.Model):
+   name = models.CharField(max_length=300)
+   description = models.TextField()
+   price = models.DecimalField(max_digits=6, decimal_places=2)
+   category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
+   date_and_time = models.DateTimeField()
+   duration = models.DurationField()
+   is_access = models.BooleanField(default=False)
+   available = models.BooleanField(default=True)
+
+   def __str__(self):
+        return self.name
+
+
+
