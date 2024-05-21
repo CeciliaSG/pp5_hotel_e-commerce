8fc676797845d0db6daa23e9d22ad51d93eeb34d
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-05-21 06:27:47 +0000

    Add styling to car and and add date and time picker to spa booking form

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ https://stackoverflow.com/questions/66814402/python-django-editing-inlineformset
 https://stackoverflow.com/questions/3367741/inlineformset-factory-composed-of-modelform
 
 https://stackoverflow.com/questions/68382186/django-add-form-validation-to-inlineformset-factory
+
+https://stackoverflow.com/questions/62315482/django-day-and-time-picker/62316055#62316055
+
+https://stackoverflow.com/questions/54825483/combine-django-filter-datetimefromtorangefilter-and-datetimeinputdatepicker
--- booking/forms.py
+++ booking/forms.py
@@ -8,10 +8,19 @@ class SpaBookingForm(forms.ModelForm):
         model = SpaBooking
         fields = ['customer_name', 'email', 'phone_number', 'date_and_time']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['date_and_time'].widget = forms.DateTimeInput(attrs={
+            'class': 'formInputs',
+            'placeholder': 'Select date and time',
+            'type': 'datetime-local'
+        })
+
 class SpaBookingServicesForm(forms.ModelForm):
     class Meta:
         model = SpaBookingServices
         fields = ['spa_service', 'quantity']
 
-
-SpaBookingServicesFormSet = inlineformset_factory(SpaBooking, SpaBookingServices, form=SpaBookingServicesForm, extra=1, can_delete=True)
+SpaBookingServicesFormSet = inlineformset_factory(
+    SpaBooking, SpaBookingServices, form=SpaBookingServicesForm, extra=1, can_delete=True
+)
