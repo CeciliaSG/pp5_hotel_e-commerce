5e6aabf45323791822a4836599c17f0a26e3765e
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-05-30 09:15:49 +0000

    make changes to checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,11 +7,21 @@ class SpaBookingForm(forms.ModelForm):
         fields = ['customer_name', 'email', 'phone_number']
 
     def __init__(self, *args, **kwargs):
-
         super().__init__(*args, **kwargs)
         placeholders = {
-        'customer_name': 'Customer Name',
-        'email': 'Email Address',
-        'phone_number': 'Phone Number',
+            'customer_name': 'Customer Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
         }
+
+        self.fields['customer_name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False 
 
