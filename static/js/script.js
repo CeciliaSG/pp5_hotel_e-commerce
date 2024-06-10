424853db568ad669a619e1889ee5c9b3bd883b95
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-06-10 06:48:49 +0000

    Add changes to scroll script for navbar, logo and navbar-button/icon

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     let stickyNavbar = document.querySelector('.sticky-navbar');
     let logo = document.querySelector('.logo');
+    let navbarButton = document.querySelector('.navbar-button');
+    let navbarButtonIcon = document.querySelector('.navbar-button i.fa-bars');
 
     if (!stickyNavbar) {
         console.error("Sticky navbar element not found");
+        return;
+    }
+
+    if (!logo) { 
+        console.error("Logo element not found");
+        return;
+    }
+
+    if (!navbarButton) { 
+        console.error("Navbar button element not found");
+        return;
+    }
+
+    if (!navbarButtonIcon) {
+        console.error("Navbar button icon element not found");
         return;
     }
 
@@ -13,9 +30,13 @@ document.addEventListener("DOMContentLoaded", function() {
         if (window.scrollY > 100) {
             stickyNavbar.style.backgroundColor = 'rgba(25, 21, 33, 1)';
             logo.style.color = 'white';
+            navbarButton.style.color = 'white';
+            navbarButtonIcon.style.color = 'white'; 
         } else {
             stickyNavbar.style.backgroundColor = 'rgba(25, 21, 33, 0.1)';
-            logo.style.color = 'black';
+            logo.style.color = '#191521';
+            navbarButton.style.color = '#191521';
+            navbarButtonIcon.style.color = '#191521';  
         }
     });
 });
