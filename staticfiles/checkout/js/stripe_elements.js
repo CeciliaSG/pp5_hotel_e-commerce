9bcc971013d4032b75f740af7dddc2e1a72f4d7e
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-06-11 10:02:52 +0000

    Uncomment code and push to Herok to identify issue with deploymen and collect static files

--- staticfiles/checkout/js/stripe_elements.js
+++ staticfiles/checkout/js/stripe_elements.js
@@ -8,25 +8,86 @@
     https://stripe.com/docs/stripe-js
 */
 
-
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var style = {
-    base: {
-        color: '#191521',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#191521'
+$(document).ready(function() {
+    let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+    let clientSecret = $('#id_client_secret').text().slice(1, -1);
+    let stripe = Stripe(stripePublicKey);
+    let elements = stripe.elements();
+    let style = {
+        base: {
+            color: '#191521',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+                color: '#191521'
+            }
+        },
+        invalid: {
+            color: '#dc3545',
+            iconColor: '#dc3545'
         }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
-};
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+    };
+    let card = elements.create('card', { style: style });
+    card.mount('#card-element');
+
+    card.addEventListener('change', function(event) {
+        let errorDiv = document.getElementById('card-errors');
+        if (event.error) {
+            let html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${event.error.message}</span>
+            `;
+            $(errorDiv).html(html);
+        } else {
+            errorDiv.textContent = '';
+        }
+    });
+
+    let form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(ev) {
+        ev.preventDefault();
+        card.update({ 'disabled': true });
+        $('#submit-button').attr('disabled', true);
+
+        let saveInfo = Boolean($('#save-info').attr('checked'));
+        let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        let postData = {
+            'csrfmiddlewaretoken': csrfToken,
+            'client_secret': clientSecret,
+            'save_info': saveInfo,
+        };
+        let url = '/checkout/cache_checkout_data/';
+
+        $.post(url, postData).done(function() {
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: $.trim(form.customer_name.value),
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                    }
+                }
+            }).then(function(result) {
+                if (result.error) {
+                    let errorDiv = document.getElementById('card-errors');
+                    let html = `
+                        <span class="icon" role="alert">
+                            <i class="fas fa-times"></i>
+                        </span>
+                        <span>${result.error.message}</span>`;
+                    $(errorDiv).html(html);
+                    card.update({ 'disabled': false });
+                    $('#submit-button').attr('disabled', false);
+                } else {
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit();
+                    }
+                }
+            });
+        });
+    });
+});
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,21 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var stickyNavbar = document.querySelector('.sticky-navbar');
+    let stickyNavbar = document.querySelector('.sticky-navbar');
+    let logo = document.querySelector('.logo');
+    let navbarButton = document.querySelector('.navbar-button');
+    let navbarButtonIcon = document.querySelector('.navbar-button i.fa-bars');
 
     if (!stickyNavbar) {
         console.error("Sticky navbar element not found");
         return;
     }
 
+    if (!logo) { 
+        console.error("Logo element not found");
+        return;
+    }
+
+    if (!navbarButton) { 
+        console.error("Navbar button element not found");
+        return;
+    }
+
+    if (!navbarButtonIcon) {
+        console.error("Navbar button icon element not found");
+        return;
+    }
+
     console.log("Sticky navbar script loaded");
 
     window.addEventListener('scroll', function() {
-        console.log("Scroll event detected");
         if (window.scrollY > 100) {
             stickyNavbar.style.backgroundColor = 'rgba(25, 21, 33, 1)';
-            console.log("Navbar set to fully opaque");
+            logo.style.color = 'white';
+            navbarButton.style.color = 'white';
+            navbarButtonIcon.style.color = 'white'; 
         } else {
-            stickyNavbar.style.backgroundColor = 'rgba(25, 21, 33, 0.5)';
-            console.log("Navbar set to more opaque");
+            stickyNavbar.style.backgroundColor = 'rgba(25, 21, 33, 0.1)';
+            logo.style.color = '#191521';
+            navbarButton.style.color = '#191521';
+            navbarButtonIcon.style.color = '#191521';  
         }
     });
 });
