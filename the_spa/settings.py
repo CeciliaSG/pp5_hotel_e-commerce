6fd07ca2f878bc8b343e56005b0966b9640271da
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-06-11 14:08:16 +0000

    Change to settings.py

--- the_spa/settings.py
+++ the_spa/settings.py
@@ -117,6 +117,7 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
@@ -212,7 +213,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 #if 'DEVELOPMENT' in os.environ:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
     #DEFAULT_FROM_EMAIL = 'thespa@example.com'
 #else:
    #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
