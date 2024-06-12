7585fb698381d478d284edad2bf64c23841c4f43
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-06-12 11:25:58 +0000

    Fix code in checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -157,9 +157,11 @@ def checkout(request):
                 selected_datetime = timezone.make_aware(selected_datetime)
                 
                 request.session['save_info'] = 'save-info' in request.POST
-                return redirect(reverse('checkout_success', kwargs={'booking_number': spa_booking.booking_number}))
+                
         else:
             messages.error(request, 'There was an error with your form. Please double check your information.')
+
+            return redirect(reverse('checkout_success', kwargs={'booking_number': spa_booking.booking_number}))
 
     else:
         spa_booking_form = SpaBookingForm()
