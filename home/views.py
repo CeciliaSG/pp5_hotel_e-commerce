576d83d8bcd3b64082eed25bf85159c610bf76f1
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-06-24 18:41:46 +0000

    Check and adjust coden with python Linter for Home

--- home/views.py
+++ home/views.py
@@ -8,26 +8,30 @@ from .forms import ContactForm
 
 # Create your views here.
 
+
 def index(request):
     """
     Render the index page of The Spa Stockholm.
 
-    This view renders the index page, which serves as the main entry point
-    for visitors to The Spa Stockholm's website. It retrieves context data
-    using utility functions to display booking information and available spa services.
+    This view renders the index page, which serves as
+    the main entry point for visitors to The Spa Stockholm's
+    website. It retrieves context data using utility functions
+    to display booking information and available spa services.
 
-    The `book_spa_service` function is called to populate the 'booking_context' variable,
-    providing information about current spa service bookings or availability.
+    The `book_spa_service` function is called to populate the
+    'booking_context' variable, providing information about
+    current spa service bookings or availability.
 
-    The `spa_services` function is called to populate the 'services_context' variable,
-    providing details about the spa's available services, packages, and treatments.
+    The `spa_services` function is called to populate the
+    'services_context' variable, providing details about
+    the spa's available services, packages, and treatments.
 
     Args:
         request (HttpRequest): The HTTP request object.
 
     Returns:
-        HttpResponse: The HTTP response object with the rendered index page,
-                      displaying booking information and spa services.
+        HttpResponse: The HTTP response object with the rendered
+        index page, displaying booking information and spa services.
     """
 
     booking_context = book_spa_service(request)
@@ -45,19 +49,22 @@ def contact(request):
     """
     Handle the contact form submission.
 
-    This view processes the contact form, which allows users to send messages to the spa.
-    If the request method is POST, it validates the form data and, if valid, sends an email
-    containing the message details to the spa's email address. After a successful form submission,
-    the user is redirected to a success page.
+    This view processes the contact form, which allows
+    users to send messages to the spa. If the request method is POST,
+    it validates the form data and, if valid, sends an email
+    containing the message details to the spa's email address.
+    After a successful form submission, the user is redirected to
+    a success page.
 
-    If the request method is GET, it initialises an empty contact form and renders the contact
-    page with the form.
+    If the request method is GET, it initialises an empty contact
+    form and renders the contact page with the form.
 
     Args:
         request (HttpRequest): The HTTP request object.
 
     Returns:
-        HttpResponse: The HTTP response object with the rendered contact page or a redirect to the success page.
+        HttpResponse: The HTTP response object with the rendered
+        contact page or a redirect to the success page.
     """
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -83,15 +90,19 @@ def contact_success(request):
     """
     Render the contact form submission success page.
 
-    This view handles the HTTP GET request to display the contact form submission success page.
-    After successfully submitting the contact form on the spa's website, users are redirected
-    to this page to inform them that their message has been successfully sent.
+    This view handles the HTTP GET request to display
+    the contact form submission success page.
+    After successfully submitting the contact form on
+    the spa's website, users are redirected
+    to this page to inform them that their message
+    has been successfully sent.
 
     Args:
         request (HttpRequest): The HTTP request object.
 
     Returns:
-        HttpResponse: The HTTP response object with the rendered contact success page.
+        HttpResponse: The HTTP response object with
+        the rendered contact success page.
     """
     return render(request, 'home/contact_success.html')
 
@@ -100,18 +111,22 @@ def about(request):
     """
     Render the "About The Spa Stockholm" page.
 
-    This view handles the HTTP GET request to display the "About The Spa Stockholm" page,
-    which provides detailed information about the spa's philosophy, facilities, treatments,
-    exclusive packages, commitment to service, and career opportunities.
+    This view handles the HTTP GET request to display
+    the "About The Spa Stockholm" page, which provides
+    detailed information about the spa's philosophy,
+    facilities, treatments, exclusive packages, commitment
+    to service, and career opportunities.
 
-    The HTML template 'spa/about.html' is used to render this page, containing comprehensive
-    content that informs visitors about the spa and encourages potential employees to join
-    the team.
+    The HTML template 'spa/about.html' is used to render
+    this page, containing comprehensive content that informs
+    visitors about the spa and encourages potential employees
+    to join the team.
 
     Args:
         request (HttpRequest): The HTTP request object.
 
     Returns:
-        HttpResponse: The HTTP response object with the rendered "About The Spa Stockholm" page.
+        HttpResponse: The HTTP response object with
+        the rendered "About The Spa Stockholm" page.
     """
     return render(request, 'home/about.html')
