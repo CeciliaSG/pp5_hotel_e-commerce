8b429221307b7df5a469dd954df1f8f9e088acf8
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-08-30 11:53:48 +0000

    Change pre_delete signal to delete spa_services rather than the spa_booking

--- services/signals.py
+++ services/signals.py
@@ -1,31 +1,30 @@
 from django.db.models.signals import post_delete, pre_delete
 from django.dispatch import receiver
 from .models import SpecificDate, Availability, TimeSlotAvailability, TimeSlot
-from booking.models import SpaBooking
+from booking.models import SpaBooking, SpaBookingServices
 
 
-@receiver(pre_delete, sender=SpaBooking)
-def pre_delete_spa_booking(sender, instance, **kwargs):
+@receiver(pre_delete, sender=SpaBookingServices)
+def pre_delete_spa_booking_service(sender, instance, **kwargs):
+    """
+    Signal to update the associated time slots' availability when a service is deleted or canceled.
+    """
+    selected_date = instance.date_and_time.date()
 
-    related_services = instance.spa_booking_services.all()
+    try:
+        time_slot = TimeSlot.objects.get(spa_service=instance.spa_service, time=instance.date_and_time.time())
+    except TimeSlot.DoesNotExist:
+        return
 
-    for service in related_services:
-        selected_date = service.date_and_time.date()
-
-        try:
-            time_slot = TimeSlot.objects.get(spa_service=service.spa_service, time=service.date_and_time.time())
-        except TimeSlot.DoesNotExist:
-            continue
-
-        try:
-            specific_date = SpecificDate.objects.get(date=selected_date)
-            TimeSlotAvailability.objects.filter(
-                specific_date=specific_date,
-                time_slot=time_slot,
-                availability__spa_service=service.spa_service
-            ).update(is_available=True, is_booked=False)
-        except (SpecificDate.DoesNotExist, Availability.DoesNotExist):
-            pass
+    try:
+        specific_date = SpecificDate.objects.get(date=selected_date)
+        TimeSlotAvailability.objects.filter(
+            specific_date=specific_date,
+            time_slot=time_slot,
+            availability__spa_service=instance.spa_service
+        ).update(is_available=True, is_booked=False)
+    except (SpecificDate.DoesNotExist, Availability.DoesNotExist):
+        pass
 
 
 
