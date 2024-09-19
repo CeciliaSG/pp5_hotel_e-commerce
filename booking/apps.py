3d63112d25dc65593934d6f18e20924523d4d847
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-09-19 08:55:14 +0000

    Add docstring to booking apps.py

--- booking/apps.py
+++ booking/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class BookingConfig(AppConfig):
+    """
+    Configuration class for the 'booking' Django app.
+
+    Sets the default auto field type and specifies the app's name.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
