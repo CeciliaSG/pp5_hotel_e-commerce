c35d632f3c03c16a2760b502f750b8e15fffe81a
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-09-19 08:44:55 +0000

    Add docstring to apps.py in checkout

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,12 +2,15 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """
+    Configuration class for the 'checkout' Django app.
+
+    Sets the default auto field type and imports signals when the app is ready.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkout'
-
-
-class CheckoutConfig(AppConfig):
     name = 'checkout'
 
     def ready(self):
         import checkout.signals
+
--- home/apps.py
+++ home/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """
+    Configuration class for the 'home' Django app.
+
+    Sets the default auto field type and specifies the app's name.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
